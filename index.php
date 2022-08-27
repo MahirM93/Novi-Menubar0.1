7a13aa8f790763f7debe34ef3033b5a262eb9b34
rails s <mesanovicmahir@gmail.com>
2022-08-27 17:01:03 +0200

    init

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<title>Jednostavni Menubar</title>
+<link rel="stylesheet"href="style.css">
+
+
+<body>
+    <header class="menu">
+
+<div class="logo">
+    <a href="index.php">
+        <img src="slika/r.png">
+</a>
+</div>
+<nav class="menu-glavni">
+    <ul>
+        <li><a href="#">Home</li>
+        <li><a href="#">O meni</li>
+        <li><a href="#">O tebi</li>
+        <li><a href="#">Svasta</li>
+        <li><a href="#">Yo</li>
+
+    </ul>
+</nav>
+</header>
+</body>
+
+
+
+</html>
